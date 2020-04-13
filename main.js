ba4613a3afbafa48e725c8c42a0540286c4b8238
Tiago G. Meurer <tiagogilvan@hotmail.com>
2020-04-13 06:12:47 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+ function createBox() {
+    var boxElement = document.createElement('div');
+    boxElement.setAttribute('class', 'box'); 
+ 
+
+    var criarElemento = document.querySelector('#app');
+    criarElemento.appendChild(boxElement);
+
+}
+
